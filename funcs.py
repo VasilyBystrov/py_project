85b4b0c467ce4c717207dbc408e35486f50b1035
Vasily Bystrov <bistrov2513@gmail.com>
2024-04-21 11:31:29 +0500

    funcs 2

--- funcs.py
+++ funcs.py
@@ -3,3 +3,10 @@ def adding_numbers(a=int(input()), b=int(input())):
 
 
 print(adding_numbers())
+
+
+def adding_numbers(a=int(input()), b=int(input())):
+    return a - b
+
+
+print(adding_numbers())
